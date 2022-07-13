e4913a68bbc3e591e25471ee09796fda78fb7d35
Jacob Good <jacobegood@gmail.com>
2022-07-14 06:26:34 +0800

    client not frontend

--- backend/app.js
+++ backend/app.js
@@ -20,14 +20,13 @@ mongoose.connect(dbURI,)
     .catch((err) => console.log(err))
 
 // routes 
-app.use('/login', authRoutes);
-app.use('/signup', authRoutes);
+app.use(authRoutes);
 
 // Serve frontend
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
 );
   
 
