82a2d0a68928f2f97ace8e3abdb41d4873d84052
Jacob Good <jacobegood@gmail.com>
2022-07-15 11:59:51 +0800

    day 2 progress

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(authRoutes);
 // Serve frontend
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
 );
   
 
