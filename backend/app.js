0995dab806241969766a40c80910d35e5d8746d5
Jacob Good <jacobegood@gmail.com>
2022-07-15 11:44:06 +0800

    day 2

--- backend/app.js
+++ backend/app.js
@@ -5,26 +5,24 @@ const authRoutes = require('./auth')
 const cors = require('cors')
 
 const port = process.env.PORT || 5001
-
 const app = express()
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors())
-
-// // database connection 
+// database connection 
 const dbURI = 'mongodb+srv://jacobegood:jacobegood@goodcluster.czb5vne.mongodb.net/movie-mania'
 mongoose.connect(dbURI,)
     .then(console.log('app is connect to mongoDB'))
     // .then((result) => app.listen(port))
     .catch((err) => console.log(err))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
 // routes 
 app.use(authRoutes);
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, '../client/build')));
-
 app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
 );
