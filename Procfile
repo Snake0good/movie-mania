a632f1b36f2fdc671bd08563547a9fb8d7452604
Jacob Good <jacobegood@gmail.com>
2022-07-17 07:36:19 +0800

    please work

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:node backend/app.js
+web: npm start
--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(authRoutes);
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, '../client', 'build')));
-app.get('/*', (req, res) =>
+app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
 );
   
